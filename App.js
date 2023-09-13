0a79fd38ed1a5be5fc89ccc463485602b6eaed4d
tommi20128 <70171019+tommi20128@users.noreply.github.com>
2023-09-13 14:33:32 +0300

    tehtävä 3

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar 
+        style="auto"
+        backgroundColor='red'
+        //hidden={true} 
+        />
     </View>
   );
 }
@@ -14,7 +19,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0,
+  }
 });
